7d7d5950f5c6b0fa76c02126a28a62d91e76ed20
loukitzanna <loukianova@email.arizona.edu>
2015-04-09 12:23:32 -0700

    analysis that almost woks
    couple not callable, ln 46

---
+++ apireader/analyze.py
@@ -0,0 +1,67 @@
+import requests
+import sqlite3 as lite
+import generatelookup as lookup
+from collections import Counter
+
+con = lite.connect('urfchampitems.db')
+champdict = {}
+
+# TODO name this function
+def thing(champID):
+    # find all rows with that ID
+    c = Counter()
+
+    with con:
+        print("found database")
+        cur = con.cursor()
+
+        sql = "SELECT item0, item1, item2, item3, item4, item5, item6  FROM champs WHERE champid = '%d' LIMIT 100" %champID
+        if True:
+        # try:
+            # Execute the SQL command
+            cur.execute(sql)
+            # Fetch all the rows in a list of lists.
+            results = cur.fetchall()
+            for row in results: # row = id, item1, item2, item 3..... item6
+                #print "LN " , row
+                for i in range(0, len(row)):
+                    if row[i] != 0:
+                        item = row[i]
+                        c[item] +=1
+                        # print "added",
+
+        # except:
+        #     print "Error: unable to fetch data"
+
+
+    # keep frequency of items
+
+    # return list of most frequent items
+    return c.most_common(7)
+
+def translate(array):
+
+    for item in array:
+        print item,
+        print lookup.get_item(item(0))
+
+    return [lookup.get_item(item(0)) for item in array]
+
+
+def generatetable():
+    # run thing once for every champion, make a static lookup dictionary
+    champlist = lookup.maketable()
+    #print champlist[62]
+    for key, value in champlist.items():
+        print value
+        champdict[value]= translate(thing(key))
+        print "name", value, "items", champdict[value]
+
+#    lookup.pack(champdict, "champdict.api")
+
+
+####### MAIN
+
+print "Champion         Items"
+generatetable()
+
--- apireader/generatelookup.py
+++ apireader/generatelookup.py
@@ -1,38 +1,13 @@
 import requests
 import sqlite3 as lite
-#import pickle
+import pickle
 import json
 
 # TODO make API calls to looks up champions by ID
 
-champtable = {}
+champlist = {}
 con = lite.connect('urfchampitems.db')
 
-
-
-#
-# def maketable():
-#     with con:
-#         print("found database")
-#         cur = con.cursor()
-#
-#         sql = "SELECT * FROM champs"
-#         try:
-#             # Execute the SQL command
-#             cur.execute(sql)
-#             # Fetch all the rows in a list of lists.
-#             results = cur.fetchall()
-#             for row in results:
-#                 id = row[0]
-#                 # make API calls
-#                 champs = {'api_key': "c73fb9af-146f-47fa-ac4f-c2b0f43ac35e"}
-#                 r = requests.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/'+id, params=champs)
-#                 dict = json.load(r)
-#                 champtable[id]=dict[name]
-#
-#         except:
-#             print "Error: unable to fetch data"
-#     return champtable
 
 def get_champ(number):
 
@@ -45,31 +20,82 @@ def get_champ(number):
 
 
 def get_item(number):
-
+    #print number
     items = {'api_key': "c73fb9af-146f-47fa-ac4f-c2b0f43ac35e"}
     r = requests.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/item/'+str(number), params=items)
     #print r.json()
     dict = r.json()
-
+    #print dict['name']
     return dict['name']
 
-def testing():
-    print "Trying to get champion 60 (Elise): ", get_champ(60)
-    print "Trying to get item 1079 (Doran's Shield): ", get_item(1054)
+
+# def testing():
+#     print "Trying to get champion 60 (Elise): ", get_champ(60)
+#     print "Trying to get item 1079 (Doran's Shield): ", get_item(1054)
 
 
 def test(n1, n2):
     print "Trying to get champion",n1,":", get_champ(n1)
     print "Trying to get item", n2, ":", get_item(n2)
-#
-#
-# def pack():
-#     pickle.dump(champtable, open("lookup.table"))
-#
-#
-# def unpack():
-#     global champtable
-#     champtable = pickle.load("lookup.table")
 
-testing()
-test(62, 3143)
+
+def pack(thing, name):
+    pickle.dump(thing, open(name))
+
+
+def unpack(name):
+    thing = pickle.load(name)
+    return thing
+
+
+def maketable():
+    with con:
+        #print("found database")
+        cur = con.cursor()
+
+        sql = "SELECT * FROM champs"
+        # try:
+        if True:
+            # Execute the SQL command
+            cur.execute(sql)
+            # Fetch all the rows in a list of lists.
+            #results = cur.fetchall()
+
+            while len(champlist) < 123: #TODO get actual number of champs
+                row = cur.fetchone()
+                #print row, type(row)
+                name = row[0]
+                if name not in champlist:
+                    champnum = row[0]
+                    #print champnum
+                    champlist[champnum] = get_champ(champnum)
+                    #print "name", name, "champ", champlist[name]
+
+            #pack(champlist, "champlist.api")
+
+            # for row in results:
+            #     id = row[0]
+            #     # make API calls
+            #     champs = {'api_key': "c73fb9af-146f-47fa-ac4f-c2b0f43ac35e"}
+            #     r = requests.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/'+id, params=champs)
+            #     dict = json.load(r)
+            #     champtable[id]=dict[name]
+
+
+
+        # except:
+        #     print "Error: unable to fetch data"
+    print "made table"
+    return champlist
+
+
+############## MAIN ####################
+#testing()
+#test(62, 3143)
+
+
+
+
+
+
+
